2dec38774ada5abe5b6023275197666729c476db
Style926 <tstyle926@gmail.com>
2024-04-12 19:06:40 -0700

    fourth commit

--- asciiArt.js
+++ asciiArt.js
@@ -1,6 +1,16 @@
 const { argv } = require('process');
-
+var figlet = require("figlet");
 // print process.argv
 
-    
-console.log(`${argv[2]}`);
+// console.log(`${argv[2]}`);
+const text = argv[2];
+
+
+figlet(text, function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
